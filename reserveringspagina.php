abe5e4e7ac89bd38abff84cfe40d04d9516b9df1
surelynobody161 <surelynobody161@gmail.com>
2024-01-15 23:46:16 +0100

    reserveringspagina front-end gemaakt alleen de links moeten nog locaties krijgen en details moeten nog toegevoegd worden zoals socials en contact bij de footer

--- reserveringspagina.php
+++ reserveringspagina.php
@@ -2,68 +2,78 @@
 ?>
 <!doctype html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-        <link rel="stylesheet" href="css/style.css">
-    </head>
-    <nav class="navigatie">
-        <img src="images/ada_logo.png" alt="">
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="stylereserveringspagina.css">
+</head>
+
+<body>
+<nav>
+    <div class="imgnav">
+        <img src="images/adatextiel-logo.png" alt="">
+    </div>
+    <div class="navigatie">
         <a href="">home</a>
         <a href="">over ons</a>
         <a href="">producten</a>
         <a href="">duurzaamheid</a>
         <a href="">reserveringen</a>
-    </nav>
+        <div class="search-container">
+            <input type="search" name="" id="search" placeholder="search"">
+            <span class=" icon is-small is-left"><i "></i></span>
+        </div>
+    </div>
+</nav>
 
-<body>
+<div class=" white-background">
+</div>
+<div class="gray-background"></div>
+
+<section>
+    <p class="zin">
+        Vul aub ons contactformulier hieronder in als u geintresseerd bent in onze producten en meer
+        informatie wilt
+    </p>
+</section>
+
 <form action="verzend adres nog niet aangemaakt">
-    <div class="samen">
-        <div class="onderelkaar1">
-            <label for="first-name">Voornaam</label>
-            <input type="text" name="frst-name" required id="first-name">
-        </div>
-        <div class="onderelkaar2">
-            <label for="last-name">Achternaam</label>
-            <input type="text" name="last-name" id="last-name" required>
-        </div>
-    </div>
-    <div class="onderelkaar3">
-        <div class="emailboven">
-            <label for="email">Email</label>
-            <input type="email" name="email" id="email" required>
-        </div>
-    </div>
-    <div class="samen1">
-        <div class="telefoonummer">
-            <label for="telefoonummer">telefoonummer</label>
-            <input type="text" name="telefoonummer" id="telefoonummer" required>
-        </div>
-    </div>
-    <div class="onderelkaar4">
-        <div class="bericht">
-            <label for="bericht">bericht</label>
-            <label for="">bericht</label>
-            <input type="" name="" id="">
+    <div class="geheel">
+        <div class="linkerkant">
+            <div class="onderelkaar1">
+                <label for="first-name"><strong>Voornaam*</strong></label>
+                <input type="text" name="frst-name" required id="first-name" placeholder="Voornaam">
+            </div>
+            <div class="onderelkaar2">
+                <label for="last-name"><strong>Achternaam*</strong></label>
+                <input type="text" name="last-name" id="last-name" placeholder="Achternaam">
+            </div>
 
+            <div class="onderelkaar3">
+                <label for="email"><strong>Email*</strong></label>
+                <input type="email" name="email" id="email" placeholder="Email">
+            </div>
+
+            <div class="onderelkaar4">
+                <label for="telefoonummer"><strong>Telefoonummer*</strong></label>
+                <input type="text" name="telefoonummer" id="telefoonummer" placeholder="Telefoonummer">
+            </div>
+        </div>
+        <div class="rechterkant">
+            <div class="onderelkaar5">
+                <label for="bericht"><strong>Bericht</strong></label>
+                <textarea name="bericht" id="bericht" required cols="30" rows="10"
+                          style="width: 395px; height: 417px;"></textarea>
+            </div>
         </div>
     </div>
-    <div class="onderelkaar5">
-        <div class="berichtflex">
-            <label for="bericht">bericht</label>
-            <textarea name="bericht" id="bericht" required cols="30" rows="10"></textarea>
-        </div>
-    </div>
+</form>
 
 
-    </div>
+</div>
 </body>
 
 </html>
