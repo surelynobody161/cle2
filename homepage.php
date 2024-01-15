e551c8dc6934a52b8470d74e45695c54229a4507
darielawerleman <1084588@hr.nl>
2024-01-15 13:06:30 +0100

    icons

--- homepage.php
+++ homepage.php
@@ -1,14 +1,14 @@
 <?php
-
 ?>
 <!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="style.css">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 <div class="center-container">
@@ -36,16 +36,20 @@
     <section class="firstSection">
         <div class="firstIcon">
             <p>WERKNEMERS</p>
+            <img src="images/icon1.png">
 
         </div>
         <div class="secondIcon">
             <p>TON GRONDSTOF DAGELIJKS VERWERKT</p>
+            <img src="images/icon2.png">
         </div>
         <div class="thirdIcon">
             <p>EINDPRODUCTEN</p>
+            <img src="images/icon3.png">
         </div>
         <div class="fourthIcon">
             <p>JAAR OP DE MARKT</p>
+            <img src="images/icon4.png">
         </div>
 
     </section>
