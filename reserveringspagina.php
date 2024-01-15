80bff7e8bb9d0043c15d20cad069c711fdd00ab2
surelynobody161 <surelynobody161@gmail.com>
2024-01-15 12:12:58 +0100

    start reservingspagina

--- reserveringspagina.php
+++ reserveringspagina.php
@@ -8,8 +8,62 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-</head>
-<body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <nav class="navigatie">
+        <img src="images/ada_logo.png" alt="">
+        <a href="">home</a>
+        <a href="">over ons</a>
+        <a href="">producten</a>
+        <a href="">duurzaamheid</a>
+        <a href="">reserveringen</a>
+    </nav>
 
+<body>
+<form action="verzend adres nog niet aangemaakt">
+    <div class="samen">
+        <div class="onderelkaar1">
+            <label for="first-name">Voornaam</label>
+            <input type="text" name="frst-name" required id="first-name">
+        </div>
+        <div class="onderelkaar2">
+            <label for="last-name">Achternaam</label>
+            <input type="text" name="last-name" id="last-name" required>
+        </div>
+    </div>
+    <div class="onderelkaar3">
+        <div class="emailboven">
+            <label for="email">Email</label>
+            <input type="email" name="email" id="email" required>
+        </div>
+    </div>
+    <div class="samen1">
+        <div class="telefoonummer">
+            <label for="telefoonummer">telefoonummer</label>
+            <input type="text" name="telefoonummer" id="telefoonummer" required>
+        </div>
+    </div>
+    <div class="onderelkaar4">
+        <div class="bericht">
+            <label for="bericht">bericht</label>
+            <label for="">bericht</label>
+            <input type="" name="" id="">
+
+        </div>
+    </div>
+    <div class="onderelkaar5">
+        <div class="berichtflex">
+            <label for="bericht">bericht</label>
+            <textarea name="bericht" id="bericht" required cols="30" rows="10"></textarea>
+        </div>
+    </div>
+
+
+    </div>
 </body>
+
 </html>
