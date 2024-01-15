20dc72b2c0e7c24d8e6ce3c2126009241f8661d4
darielawerleman <1084588@hr.nl>
2024-01-15 12:16:23 +0100

    home page

--- homepage.php
+++ homepage.php
@@ -10,6 +10,67 @@
     <title>Document</title>
 </head>
 <body>
+<div class="center-container">
+    <nav>
+        <div class="left">
+            <img class="navImg" src="images/adatextiel-logo.png" alt="logo">
+        </div>
+        <div class="right">
+            <a href="#home">HOME</a>
+            <a href="">OVER ONS</a>
+            <a href="">PRODUCTEN</a>
+            <a href="">DUURZAAMHEID</a>
+            <a href="">CONTACT</a>
+        </div>
+
+    </nav>
+</div>
+
+
+<header>
+    <h1> Welkom op onze Website</h1>
+</header>
+
+<main>
+    <section class="firstSection">
+        <div class="firstIcon">
+            <p>WERKNEMERS</p>
+        </div>
+        <div class="secondIcon">
+            <p>TON GRONDSTOF DAGELIJKS VERWERKT</p>
+        </div>
+        <div class="thirdIcon">
+            <p>EINDPRODUCTEN</p>
+        </div>
+        <div class="fourthIcon">
+            <p>JAAR OP DE MARKT</p>
+        </div>
+
+    </section>
+
+    <h2>Welkom in de wereld van tweedehands textiel</h2>
+</main>
+
+<footer>
+
+    <img class="footerimg" src="images/adalogotr.png" alt="logo">
+    <p class="footertext">Known for our continuous quality!</p>
+
+
+    <div class="footerinfo">
+        <p>+31 10 226 85 47</p>
+        <p>info@adatex.nl</p>
+        <p>Heldringstraat 39</p>
+        <p></p>
+    </div>
+
+    <div class="footerright">
+        <img src="images/niwologo.png">
+        <img src="images/kiwalogo.png">
+    </div>
+
+</footer>
+
 
 </body>
 </html>
