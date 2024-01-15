fdf8563eb3ed5602f3973cf931a49b0774998d37
darielawerleman <1084588@hr.nl>
2024-01-15 16:34:49 +0100

    over ons page

---
+++ overonspage.php
@@ -0,0 +1,57 @@
+<?php
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="style2.css">
+</head>
+<body>
+<div class="center-container">
+    <nav>
+        <div class="left">
+            <img class="navImg" src="images/adatextiel-logo.png" alt="logo">
+        </div>
+        <div class="right">
+            <a href="#home">HOME</a>
+            <a href="overonspage.php">OVER ONS</a>
+            <a href="">PRODUCTEN</a>
+            <a href="">DUURZAAMHEID</a>
+            <a href="">CONTACT</a>
+        </div>
+
+    </nav>
+</div>
+
+<header>
+    <h1>OVER ONS </h1>
+</header>
+
+<section class="firstSection">
+    <div class="about">
+
+        <p> ADA Textiel Recycling is een familiebedrijf die is opgericht in 2003. De onderneming is gespecialiseerd in het sorteren en herwaarderen van gebruikte/afgedankte textiel. Dagelijks wordt er 20 ton aan grondstoffen verwerkt. Ons doel is om een eindproduct te leveren van superieure kwaliteit. </p>
+
+        <p> Zo willen wij hiermee bijdragen aan een duurzamere wereldhandel door ervoor te zorgen dat ieder product geleverd wordt op een daarvoor geschikte bestemming. </p>
+
+       <p> Om dit juist en zorgvuldig te doen, zet ons team zich dagelijks 100% in, in het sorteerproces.
+           Met bijna 20 jaar ervaring op de markt, staan wij bekend om de consistente kwaliteit die wij leveren. </p>
+
+        <p>  Onze ervaring is ook terug te zien in de kwaliteit van onze producten, dankzij onze team. </p>
+
+    </div>
+    <img src="images/overons-about.jpeg">
+
+</section>
+
+<section class="secondSection">
+
+
+</section>
+
+</body>
+</html>
